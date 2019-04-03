13d12a9749192199df42e8b30a8471899656277a
Hidenori Miyoshi <miyohide@gmail.com>
2019-04-03 22:00:04 +0900

    FIXMEとしていたexceeded/unresolvedの部分を解消

--- lib/parseGVP/parser.rb
+++ lib/parseGVP/parser.rb
@@ -17,19 +17,24 @@ module ParseGVP
     end
 
     def detail_result
-      current = @json["current"]["count"]
-      current_lib = @json["current"]["dependencies"].map do |i|
+      results = []
+      results << %Q(- current: #{@json["current"]["count"]})
+      results << @json["current"]["dependencies"].map do |i|
         %Q(  - #{i["group"]}:#{i["name"]}:#{i["version"]})
       end.join("\n")
-      # FIXME exceededの詳細パターンを実装
-      exceeded = @json["exceeded"]["count"]
-      outdated = @json["outdated"]["count"]
-      outdated_lib = @json["outdated"]["dependencies"].map do |i|
+      results << %Q(- exceeded: #{@json["exceeded"]["count"]})
+      results << @json["exceeded"]["dependencies"].map do |i|
         %Q(  - #{i["group"]}:#{i["name"]})
       end.join("\n")
-      # FIXME unresolvedの詳細パターンを実装
-      unresolved = @json["unresolved"]["count"]
-      "- current: #{current}\n#{current_lib}\n- exceeded: #{exceeded}\n- outdated: #{outdated}\n#{outdated_lib}\n- unresolved: #{unresolved}\n"
+      results << %Q(- outdated: #{@json["outdated"]["count"]})
+      results << @json["outdated"]["dependencies"].map do |i|
+        %Q(  - #{i["group"]}:#{i["name"]})
+      end.join("\n")
+      results << %Q(- unresolved: #{@json["unresolved"]["count"]})
+      results << @json["unresolved"]["dependencies"].map do |i|
+        %Q(  - #{i["group"]}:#{i["name"]})
+      end.join("\n")
+      results.delete_if(&:empty?).join("\n") + "\n"
     end
 
     private
