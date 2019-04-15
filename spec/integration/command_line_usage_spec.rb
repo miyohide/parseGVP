7716c6f82178e0be30850c38023da3c730a7dad3
Hidenori Miyoshi <miyohide@gmail.com>
2019-04-15 20:49:31 +0900

    ファイル名を指定した場合のテストを追記

--- spec/integration/command_line_usage_spec.rb
+++ spec/integration/command_line_usage_spec.rb
@@ -1,8 +1,16 @@
 require "spec_helper"
+require "stringio"
 
 RSpec.describe ParseGVP, "running from command line" do
   it "print usage" do
     output = run_command("")
     expect(output).to include "ParseGVP Argument Error.\n"
+  end
+
+  context "only Filename" do
+    it "aaa" do
+      output = run_command("spec/test_data/report.json")
+      expect(output).to include "- current: 2\n"
+    end
   end
 end
