50d16c26a35835310487e0f233b860ddc15117bf
Hidenori Miyoshi <miyohide@gmail.com>
2019-04-22 21:39:07 +0900

    contextをキチンと書いた

--- spec/integration/command_line_usage_spec.rb
+++ spec/integration/command_line_usage_spec.rb
@@ -2,12 +2,14 @@ require "spec_helper"
 require "stringio"
 
 RSpec.describe ParseGVP, "running from command line" do
-  it "print usage" do
-    output = run_command("")
-    expect(output).to include "ParseGVP Argument Error.\n"
+  context "引数なしの場合" do
+    it "使い方の説明を出力すること" do
+      output = run_command("")
+      expect(output).to include "ParseGVP Argument Error.\n"
+    end
   end
 
-  context "only Filename" do
+  context "ファイル名だけ指定している場合" do
     it "詳細な結果が出力されること" do
       output = run_command("spec/test_data/report.json")
       expect(output).to include "- current: 2\n"
